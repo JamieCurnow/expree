3c1d78d0668ed8a9cd4095277c2eaa30c6f01a2e
JamieCurnow <jamie@island-web.ca>
2022-07-26 13:41:29 +0100

    update the tests

--- tests/parseRouteHandlers.test.ts
+++ tests/parseRouteHandlers.test.ts
@@ -1,27 +1,33 @@
 import { describe, expect, test } from 'vitest'
 import { parseRouteHandlers } from '../src/parseRouteHandlers'
-import Joi from 'joi'
+import { z } from 'zod'
 
 const middlewareFn = () => 'MiddleWare'
-const validate = (joi: Joi.Root) => ({ body: { requiredThing: joi.boolean().required() } })
+const validate = (zod: typeof z) => ({ body: zod.object({ requiredThing: zod.boolean() }) })
 const handler = () => 'Handler'
 
 describe('parseRouteHandlers', () => {
   test('should pass back the route handlers in the correct order with validation', () => {
-    const handlers = parseRouteHandlers({
-      handler,
-      middleware: [middlewareFn],
-      validate
-    })
+    const handlers = parseRouteHandlers(
+      {
+        handler,
+        middleware: [middlewareFn],
+        validate
+      },
+      z
+    )
 
     expect(handlers[1]).toEqual(middlewareFn)
   })
 
   test('should pass back the route handlers in the correct order without validation', () => {
-    const handlers = parseRouteHandlers({
-      handler,
-      middleware: [middlewareFn]
-    })
+    const handlers = parseRouteHandlers(
+      {
+        handler,
+        middleware: [middlewareFn]
+      },
+      z
+    )
 
     expect(handlers[0]).toEqual(middlewareFn)
   })
